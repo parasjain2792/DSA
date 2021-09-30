f83b85c9271f1b78ffca5c2a3774cd2d893ac6ca
Paras Jain <55691273+parasjain2792@users.noreply.github.com>
2021-09-30 13:15:12 +0530

    Update and rename Knapsack(hackerrank unbounded).txt to Knapsack(hackerrankunbounded).cpp

--- DP/Knapsack(hackerrankunbounded).cpp
+++ DP/Knapsack(hackerrankunbounded).cpp
@@ -1,5 +1,5 @@
-Qs:Given an array of integers and a target sum, determine the sum nearest to but not exceeding the target that can be created. 
-To create the sum, use any element of your array zero or more times.
+// Qs:Given an array of integers and a target sum, determine the sum nearest to but not exceeding the target that can be created. 
+// To create the sum, use any element of your array zero or more times.
 
 
 #include<bits/stdc++.h>
